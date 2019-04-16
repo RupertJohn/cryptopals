c4e42ff1fe9300147a131436f409582e92d57a7e
Rupert Saxton <rupertsaxton@gmail.com>
2019-04-16 10:42:01 -0400

    Complete challenge 2

--- set_1/set_1.go
+++ set_1/set_1.go
@@ -13,3 +13,17 @@ func HexToBase64(hexString string) (string, error) {
   encoded := b64.StdEncoding.EncodeToString([]byte(hexDecoded))
   return encoded, nil
 }
+
+func Xor(hexString1, hexString2 string) string {
+  hexDecoded1, _ := hex.DecodeString(hexString1)
+  hexDecoded2, _ := hex.DecodeString(hexString2)
+  xoredSlice := make([]byte, len(hexDecoded1))
+
+  for i := range hexDecoded1 {
+    xoredSlice[i] = hexDecoded1[i] ^ hexDecoded2[i]
+  }
+
+  xored := hex.EncodeToString(xoredSlice)
+
+  return xored
+}
