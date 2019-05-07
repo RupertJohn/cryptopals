63db36624ce4ee3ad0a17c2dde1fcf114602592d
Rupert Saxton <rupertsaxton@gmail.com>
2019-05-07 08:34:02 -0400

    Apply Go best practices according to gofmt and golint

--- set_1/set_1.go
+++ set_1/set_1.go
@@ -1,29 +1,36 @@
 package set1
 
 import (
-  b64 "encoding/base64"
-  "encoding/hex"
+	b64 "encoding/base64"
+	"encoding/hex"
 )
 
+// HexToBase64 takes a hex string and converts it to base 64
 func HexToBase64(hexString string) (string, error) {
-  hexDecoded, err := hex.DecodeString(hexString)
-  if err != nil {
-    return "", err
-  }
-  encoded := b64.StdEncoding.EncodeToString([]byte(hexDecoded))
-  return encoded, nil
+	hexDecoded, err := hex.DecodeString(hexString)
+	if err != nil {
+		return "", err
+	}
+	encoded := b64.StdEncoding.EncodeToString([]byte(hexDecoded))
+	return encoded, nil
 }
 
-func Xor(hexString1, hexString2 string) string {
-  hexDecoded1, _ := hex.DecodeString(hexString1)
-  hexDecoded2, _ := hex.DecodeString(hexString2)
-  xoredSlice := make([]byte, len(hexDecoded1))
+// xor will take two equal length byte arrays and xor the each byte against the other
+func xor(array1, array2 []byte) []byte {
+	xoredSlice := make([]byte, len(array1))
 
-  for i := range hexDecoded1 {
-    xoredSlice[i] = hexDecoded1[i] ^ hexDecoded2[i]
-  }
+	for i := range array1 {
+		j := i % len(array2)
+		xoredSlice[i] = array1[i] ^ array2[j]
+	}
 
-  xored := hex.EncodeToString(xoredSlice)
+	return xoredSlice
+}
 
-  return xored
+// HexXor will taske two hex strings and xor them against eachother
+func HexXor(hexString1, hexString2 string) string {
+	hexDecoded1, _ := hex.DecodeString(hexString1)
+	hexDecoded2, _ := hex.DecodeString(hexString2)
+	xored := hex.EncodeToString(xor(hexDecoded1, hexDecoded2))
+	return xored
 }
--- set_1/set_1_test.go
+++ set_1/set_1_test.go
@@ -3,34 +3,34 @@ package set1
 import "testing"
 
 type hexPair struct {
-  hexString     string
-  want          string
+	hexString string
+	want      string
 }
 
 type hexXor struct {
-  hexString1    string
-  hexString2    string
-  want          string
+	hexString1 string
+	hexString2 string
+	want       string
 }
 
 func TestSet1(t *testing.T) {
-  hexCase := hexPair{"49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d","SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t"}
-  b64String, _ := HexToBase64(hexCase.hexString)
-  if b64String != hexCase.want {
-    t.Error(
-      "For", hexCase.hexString,
-      "expected", hexCase.want,
-      "got", b64String,
-    )
-  }
+	hexCase := hexPair{"49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d", "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t"}
+	b64String, _ := HexToBase64(hexCase.hexString)
+	if b64String != hexCase.want {
+		t.Error(
+			"For", hexCase.hexString,
+			"expected", hexCase.want,
+			"got", b64String,
+		)
+	}
 
-  xor := hexXor{"1c0111001f010100061a024b53535009181c", "686974207468652062756c6c277320657965", "746865206b696420646f6e277420706c6179"}
-  xored := Xor(xor.hexString1, xor.hexString2)
-  if xored != xor.want {
-    t.Error(
-      "For", xor.hexString1, xor.hexString2,
-      "expected", xor.want,
-      "got", xored,
-    )
-  }
+	xor := hexXor{"1c0111001f010100061a024b53535009181c", "686974207468652062756c6c277320657965", "746865206b696420646f6e277420706c6179"}
+	xored := HexXor(xor.hexString1, xor.hexString2)
+	if xored != xor.want {
+		t.Error(
+			"For", xor.hexString1, xor.hexString2,
+			"expected", xor.want,
+			"got", xored,
+		)
+	}
 }
